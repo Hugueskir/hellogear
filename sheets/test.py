a1763b31a1b6df29ef85bf74b5bd68012a5cffb1
Hugues Kir <50173504+Hugueskir@users.noreply.github.com>
2020-02-10 12:40:30 -0500

    Add files via upload
    initial file drop

---
+++ sheets/test.py
@@ -0,0 +1,16 @@
+event = {
+    'summary': ' Papa',
+    'description': 'TNM',
+    'location': 'TNM',
+    'start': {
+        'dateTime': '2020-01-27T09:00:00',
+        'timeZone': 'America/Toronto',
+    },
+
+    'end': {
+        'dateTime': '2020-01-27T17:10:00',
+        'timeZone': 'America/Toronto',
+    }
+}
+
+print(type(event))
